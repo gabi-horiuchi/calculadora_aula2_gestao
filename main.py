28d0baba46ec824436ba1dea02804ddcdf96bf32
JVLB06 <butzenjvlb@gmail.com>
2025-08-25 20:07:18 -0300

    feat/Added limiter

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
+from time import sleep
+
 while True:
     count = 0
     count_bi_years = 0
     print("Descubra quantos anos bissextos existem de 2000 até x (Limite 4000)")
     basis_year = int(input("Insira o ano referência para o cálculo de anos bissextos:"))
 
-    for year in range(2000, basis_year):
-        if count == 4:
-            count_bi_years += 1
-            count = 0
-        count += 1 
-    
-    print(f"Até o ano de {basis_year} existe(m) {count_bi_years} ano(s) bissexto(s)")
+    if basis_year <= 4000 and basis_year >= 2000:
+        for year in range(2000, basis_year):
+            if count == 4:
+                count_bi_years += 1
+                count = 0
+            count += 1
+
+        sleep(1.5)
+        print(f"Até o ano de {basis_year} existe(m) {count_bi_years} ano(s) bissexto(s)")
+        print()
+        sleep(1.5)
+    else:
+        print("Por favor insira um valor entre 2000 e 4000") 
+        print()
+        sleep(1.5)
