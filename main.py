4779486b7348a15e2b05ff8ca5105d1c6055fd82
JVLB06 <butzenjvlb@gmail.com>
2025-08-25 20:01:59 -0300

    feat/Calculus-added

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#Centra structure of the code
+while True:
+    count = 0
+    count_bi_years = 0
+    print("Descubra quantos anos bissextos existem de 2000 até x (Limite 4000)")
+    basis_year = int(input("Insira o ano referência para o cálculo de anos bissextos:"))
+
+    for year in range(2000, basis_year):
+        if count == 4:
+            count_bi_years += 1
+            count = 0
+        count += 1 
+    
+    print(f"Até o ano de {basis_year} existe(m) {count_bi_years} ano(s) bissexto(s)")
